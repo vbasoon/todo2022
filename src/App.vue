38b95274e6f13591ac829670cc9cb5a64079171e
Vlad Basoon <vbasoon@gmail.com>
2022-01-11 08:36:04 +0200

    start

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Todo2022",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
